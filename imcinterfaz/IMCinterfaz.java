7349857bdfa541fbccfb0286cad00c9a95837664
bran-u08 <63027582+bran-u08@users.noreply.github.com>
2020-06-18 15:46:47 -0500

    Interfaz

---
+++ imcinterfaz/IMCinterfaz.java
@@ -0,0 +1,14 @@
+/*
+Solorzano Trejo Brandon Uriel
+2S11
+IMC con interfaz grafica
+ */
+package imcinterfaz;
+
+public class IMCinterfaz {
+
+    public static void main(String[] args) {
+       
+    }
+    
+}
